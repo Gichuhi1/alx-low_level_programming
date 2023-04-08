5650d87376cd85447627d49760be5ae7a02cdbe5
Gichuhi1 <alicegichuhi8@gmail.com>
2023-04-08 03:27:58 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
